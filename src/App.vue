9b50b41bef75108980740596de16de0930cd6139
Lakshman594 <lakshmankumarmattaparthi@gmail.com>
2024-02-09 17:16:12 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const count = ref(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>  
+  <h1>Increasing The Count by clicking the button</h1>
+
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2444;
+  margin-top: 60px;
+}
+
+h1{
+  
+}
+</style>
+
